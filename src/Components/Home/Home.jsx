c3eb68fc0534a7ac005e30f53340ba7038880d6f
wasifimtiaz72 <wasifimtiaz72@gmail.com>
2024-12-26 18:09:48 +0000

    First commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div>Hello World</div>
+            <div>Hello World</div>
+            <div>Hello World</div>
+            <div>Hello World</div>
+            <div>Hello World</div>
+        </>
+    )
+}
+
+export default Home
---
+++ src/Layout/App/App.js
@@ -0,0 +1,14 @@
+import Home from '../../Components/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
